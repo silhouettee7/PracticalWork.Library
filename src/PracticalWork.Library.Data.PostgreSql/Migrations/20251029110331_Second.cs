357beb666872476fd7ae68e392b157bc99f1428e
Kamil Gilfanov <kamilgilfanov6@gmail.com>
2025-10-29 21:52:40 +0300

    add second migration

---
+++ src/PracticalWork.Library.Data.PostgreSql/Migrations/20251029110331_Second.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PracticalWork.Library.Data.PostgreSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Category",
+                table: "Books",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Books");
+        }
+    }
+}
